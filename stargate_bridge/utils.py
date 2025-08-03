ccd65d6c8b97c4ce6752daaa300e53b9a55bb135
Rakib Hossain <r.rakib.7254@gmail.com>
2025-08-03 23:49:14 +0600

    added Tokens class with common token addresses for Ethereum, Polygon, and Arbitrum

---
+++ stargate_bridge/utils.py
@@ -0,0 +1,18 @@
+class Tokens:
+    """Common token addresses for different chains."""
+    
+    ETHEREUM = {
+        'USDC': '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+        'USDT': '0xdac17f958d2ee523a2206206994597c13d831ec7'
+    }
+    
+    POLYGON = {
+        'USDC': '0x3c499c542cef5e3811e1192ce70d8cc03d5c3359',
+        'USDT': '0xc2132d05d31c914a87c6611c10748aeb04b58e8f'
+    }
+    
+    ARBITRUM = {
+        'USDC': '0xaf88d065e77c8cc2239327c5edb3a432268e5831',
+        'USDT': '0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9'
+    }
+
